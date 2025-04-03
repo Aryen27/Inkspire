746c67222ddf4cdc2f577942ceafbfbfa383bb3b
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-03 17:32:50 +0530

    Initialized and configured the project

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req,res) => {
+  res.send("Hello");
+})
+
+app.listen(PORT, 'localhost', () => {
+  console.log("Server is running @ http://localhost:"+PORT);
+});
