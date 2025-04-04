7e5ee57d68d6abb0430c44ec4376b744dbbcdaa3
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-04 19:05:47 +0530

    Implemented POST /blog route to add a blog

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -33,6 +33,7 @@ router.get('/:id', async (req, res) => {
   4. Show author if logged in and vice versa
   */
   
+  // Get blog from database
   const blogId = req.params.id;
   const [results, fields] = await connection.query('SELECT * FROM blogs WHERE bid=?', [blogId]);
 
@@ -43,11 +44,26 @@ router.get('/:id', async (req, res) => {
 })
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /*
   1. Check if user has logged in.
   2. Add blog post to database.
   */
+  
+  // Adding blog to database
+  const { authorid, title, content } = req.body;
+
+  // Check if a similar blog already exists
+  let [results, fields] = await connection.query('SELECT * FROM blogs WHERE title=? AND authorid=?', [title, authorid]);
+  if (results.length != 0)
+    return res.status(400).json({ message: 'A similar blog already exists!' });
+
+  [results, fields] = await connection.query('INSERT INTO blogs(title, content, authorid) VALUES(?, ?, ?)', [title, content, authorid]);
+  console.log(results);
+  if (results.serverStatus != 2)
+    return res.status(500).json({ message: 'There a issue processing your request' });
+
+  return res.status(200).json({ message: 'Your blog has been added successfully' });
 })
 
 
