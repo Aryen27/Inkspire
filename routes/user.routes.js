2893eff371e7b0d01042d5d651dfdb8f36be774f
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-04 20:52:04 +0530

    Created and implemented PATCH/user/:id endpoint to update user details

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,5 +6,27 @@ const router = express.Router();
 
 const connection = connectionCred;
 
+router.get('/', async (req, res) => {
+  
+})
+
+router.patch('/:id', async (req, res) => {
+  const { name, email } = req.body;
+  const uid = req.params.id;
+
+  try {
+    const [results, fields] = await connection.query('UPDATE users SET name=?, email=? WHERE uid=?', [name, email, uid]);
+    
+    if (results.affectedRows == 0)
+      return res.status(404).json({ message: 'User not found' })
+    
+    return res.status(200).json({ sucess: true, message: 'User details have been updated successfully' });
+  } catch (err) {
+    console.error('Error updating user details:', err);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+})
+
+
 
 export default router;
