3b19d542e8dfc0b3f1f096d991472ee4771d56bd
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-23 05:44:55 +0530

    Added data to blog side card to show trending blogs

--- client/src/ui/BlogContainer.tsx
+++ client/src/ui/BlogContainer.tsx
@@ -5,11 +5,11 @@ function BlogConatiner({ blog }) {
   let blogDate:(string|Date) = new Date(blog.createdAt);
   blogDate = blogDate.toLocaleDateString("en-US", { day: "numeric", month: "long", year: "numeric" });
   return (
-    <div className='my-2 border-b w-full'>
+    <div className='my-2 border-b w-[90%]'>
     <h4 className='font-bold text-md'> 
     {blog.title}
     </h4>
-    <span className = 'text-slate-400 text-[0.65rem] font-normal italic' > { brief } </span>
+    <span className = 'text-slate-400 text-[0.55rem] font-normal italic' > { brief } </span>
     <div>
     <span className='text-slate-400 text-[0.5rem]'>{blogDate}</span>
     <span className='text-slate-400 text-[0.5rem] px-3'>🩷{blog.likes}     </span>
--- client/src/ui/BlogSideCard.tsx
+++ client/src/ui/BlogSideCard.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function BlogSideCard({blog}) {
-  // console.log(blog);
+  let blogDate:(string|Date) = new Date(blog.createdAt);
+  blogDate = blogDate.toLocaleDateString("en-US", { day: "numeric", month: "long", year: "numeric" });
   return (
-    <div>BlogSideCard</div>
+    <div >
+    <span className= 'font-bold text-[0.7rem]' > { blog.title } </span>
+    <span className='font-normal text-[0.5rem] text-slate-500'>{blogDate}</span>
+    </div>
   )
 }
 
